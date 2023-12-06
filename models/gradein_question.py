64350c2e1d1025146d23f1d5e1d351249a927588
Gabriela <gdcvelasquez@gmail.com>
2023-12-06 20:38:26 -0300

    T17-T18-T19_2

---
+++ models/gradein_question.py
@@ -0,0 +1,12 @@
+# models.py
+from odoo import models, fields, api
+
+class GradeInQuestion(models.Model):
+    _name = 'gradein.question'
+    _description = 'Gradein Model'
+
+    name = fields.Char(string='Name')
+    active = fields.Boolean(string='Active', default=True)
+    equipment_type_ids = fields.Many2many('gradein_equipment', string='TypeEquipment')
+    answer_ids = fields.Many2many('gradein.answer', string='Answers')
+    #hola
