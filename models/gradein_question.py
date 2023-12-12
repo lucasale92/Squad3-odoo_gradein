2929883fb9e5c81b646618e8b62b4699a3b8b323
Gabriela <gdcvelasquez@gmail.com>
2023-12-12 20:51:42 -0300

    T1S3-T17
    MODELO Y VIEW TREE

--- models/gradein_question.py
+++ models/gradein_question.py
@@ -7,6 +7,11 @@ class GradeInQuestion(models.Model):
 
     name = fields.Char(string='Name')
     active = fields.Boolean(string='Active', default=True)
+<<<<<<< Updated upstream
     equipment_type_ids = fields.Many2many('gradein_equipment', string='TypeEquipment')
     answer_ids = fields.Many2many('gradein.answer', string='Answers')
     #hola
+=======
+    equipment_type_ids = fields.Many2many('gradein.equipment.type', string='TypeEquipment')
+    answer_ids = fields.Many2many('gradein.answer', string='Answers')
+>>>>>>> Stashed changes
