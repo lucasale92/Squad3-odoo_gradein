44b81f31422f463396fda78a39d51d948734ce7c
Gabriela <gdcvelasquez@gmail.com>
2023-12-15 20:11:18 -0300

    update string question -eq

--- models/gradein_question.py
+++ models/gradein_question.py
@@ -8,5 +8,5 @@ class GradeInQuestion(models.Model):
     name = fields.Char(string='Name')
     active = fields.Boolean(string='Active', default=True)
 
-    equipment_type_ids = fields.Many2many('gradein.equipment.type', string='TypeEquipment')
+    equipment_type_ids = fields.Many2many('gradein.equipment.type', string='Type Equipment')
     answer_ids = fields.Many2many('gradein.answer', string='Answers')
