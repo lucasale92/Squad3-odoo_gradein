90c212a38e0e385b8b98fd6c77a47b3eb517abd5
Gabriela <gdcvelasquez@gmail.com>
2023-12-14 22:36:55 -0300

    update img

--- __manifest__.py
+++ __manifest__.py
@@ -8,14 +8,16 @@
     "data": [
         'security/res_groups.xml',
         'security/ir.model.access.csv',
-        'views/menu_services.xml',
         'views/gradein_rejection_motive.xml',
         'views/gradein_answer_views.xml',
         'views/gradein_question_views.xml',
         'views/gradein_equipment_type_views.xml',
         'views/gradein_equipment_views.xml',
+        'views/menu_services.xml',
         'views/gradein_order_views.xml',
         'reports/gradein_reports.xml',        
     ],
-    
+        'images': [
+        'static/description/phone.png',
+    ],
 }
